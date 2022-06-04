7d70ea200a0c9fa32258129fb1f83c24ced67063
biruk asrat <birukasrat178@gmail.com>
2022-06-04 00:48:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *   * main - Prints a text
@@ -11,13 +9,8 @@
  *       * Return: Always 0 (Success)
  *
  *         */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
